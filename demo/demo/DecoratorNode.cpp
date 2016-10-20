1c8adc502b5b26e8c257d2154dcffebb4466b7a6
Maddie <mk186442@falmouth.ac.uk>
2016-10-20 13:28:05 +0100

    Added DecoratorNode class

---
+++ demo/demo/DecoratorNode.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "DecoratorNode.h"
+
+
+DecoratorNode::DecoratorNode()
+{
+}
+
+
+DecoratorNode::~DecoratorNode()
+{
+}
+
+
---
+++ demo/demo/DecoratorNode.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Node.h"
+class DecoratorNode :
+	public Node
+{
+public:
+	DecoratorNode();
+	~DecoratorNode();
+
+
+};
+
